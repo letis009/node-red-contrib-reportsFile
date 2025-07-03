48fbe08ddb3d527669a16c390505ca8e2cc2bfc4
letis009 <65073218+letis009@users.noreply.github.com>
2025-07-03 09:24:43 +0200

    Update report-logic.js

--- report-logic.js
+++ report-logic.js
@@ -1,11 +1,2 @@
 module.exports = function(msg, RED, node) {
-    // const printStringArray = [];
-    const global = {
-        get: (key) => {
-            if (key === "prod.enabledDevice") return "ANALOGUE";
-            if (key === "prod.reportFormat") return "SBV+Rej";
-            return undefined;
-        }
-    };
-    /* Full report logic from user’s function goes here */
 };
